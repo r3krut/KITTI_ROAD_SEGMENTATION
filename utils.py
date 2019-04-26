b0ae35b1054833b3fc7ad33ea04f7566054321d3
r3krut <tananakin.mikhail@gmail.com>
2019-04-26 13:58:27 +0300

    Util for moving a Torch tensor to a GPU was added.

--- utils.py
+++ utils.py
@@ -12,3 +12,7 @@ def count_params(model: nn.Module) -> (int, int):
     total = sum(p.numel() for p in model.parameters())
     trainable = sum(p.numel() for p in model.parameters() if p.requires_grad)
     return (total, trainable)
+
+
+def to_gpu(x: torch.Tensor):
+    return x.cuda(non_blocking=True) if torch.cuda.is_available() else x
