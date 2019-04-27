240bfcb4a7b2dc8951995a7084bfc9e39aaaccf7
r3krut <tananakin.mikhail@gmail.com>
2019-04-27 19:31:35 +0300

    Some mods.

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ torch.manual_seed(111)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Argument parser for the main module. Main module represents train procedure.")
-    parser.add_argument("--root-dir", type=str, reqired=True, help="Path to the root dir.")
+    parser.add_argument("--root-dir", type=str, required=True, help="Path to the root dir.")
     parser.add_argument("--model-name", type=str, required=True, help="Name of model.")
-    parser.add_argument("--dataset-path", type=str, requered=True, help="Path to the KITTI dataset which contains 'testing' and 'training' subdirs.")
+    parser.add_argument("--dataset-path", type=str, required=True, help="Path to the KITTI dataset which contains 'testing' and 'training' subdirs.")
     
     #optimizer options
     parser.add_argument("--optim", type=str, default="SGD", help="Type of optimizer: SGD or Adam")
@@ -39,7 +39,7 @@ if __name__ == "__main__":
 
     #other options
     parser.add_argument("--n-epochs", type=int, default=100, help="Number of training epochs.")
-    parser.add_argument("--batch-szie", type=int, default=4, help="Number of examples per batch.")
+    parser.add_argument("--batch-size", type=int, default=4, help="Number of examples per batch.")
     parser.add_argument("--num-workers", type=int, default=8, help="Number of loading workers.")
     parser.add_argument("--device-ids", type=str, default="0", help="ID of devices for multiple GPUs.")
     parser.add_argument("--alpha", type=float, default=0, help="Modulation factor for custom loss.")
@@ -62,18 +62,21 @@ if __name__ == "__main__":
     #Loss definition
     loss = BCEJaccardLoss(alpha=args.alpha)
     
+    train_dataset = RoadDataset(dataset_path=Path(args.dataset_path))
+    valid_dataset = RoadDataset(dataset_path=Path(args.dataset_path), is_valid=True)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, pin_memory=True)
+    valid_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True)
+
+    print("[TRAIN IFNO] Train dataset length: {}".format(len(train_dataset)))
+    print("[TRAIN INFO] Validation dataset length: {}".format(len(valid_dataset)))
+
     #Optim definition
     if args.optim == "SGD":
-        optim = SGD(params=model.params, lr=args.lr, momentum=args.momentum)
-        print("[TRAIN INFO]: Uses the SGD optimizer with 'lr'={0} and 'momentum'={1}".format(args.lr, args.momentum))
+        optim = SGD(params=model.parameters(), lr=args.lr, momentum=args.momentum)
+        print("[TRAIN INFO] Uses the SGD optimizer with lr={0} and momentum={1}".format(args.lr, args.momentum))
     else:
-        optim = Adam(params=model.params, lr=args.lr)
-        print("[TRAIN INFO]: Uses the Adam optimizer with 'lr'={0}".format(args.lr))
-
-    train_dataset = RoadDataset(dataset_path=args.dataset_path)
-    valid_dataset = RoadDataset(dataset_path=args.dataset_path, is_valid=True)
-    train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, pin_memory=True)
-    train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True)
+        optim = Adam(params=model.parameters(), lr=args.lr)
+        print("[TRAIN INFO] Uses the Adam optimizer with lr={0}".format(args.lr))
 
     valid = validation
 
