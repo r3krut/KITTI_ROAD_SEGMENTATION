0515f885c02cee2be15f60e9e7b15a281e88b32f
r3krut <tananakin.mikhail@gmail.com>
2019-04-30 19:28:56 +0300

    shutdown_system main file was added.

---
+++ shutdown_system.py
@@ -0,0 +1,8 @@
+"""
+    This module contains only one function that allows disable the current system.
+"""
+
+from utils import shutdown_system
+
+if __name__ == "__main__":
+    shutdown_system()
