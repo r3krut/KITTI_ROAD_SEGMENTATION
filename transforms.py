3a4e48c01b327ab3ea83eca066cda18245e33947
r3krut <tananakin.mikhail@gmail.com>
2019-05-05 23:34:02 +0300

    More strongest train transforms.

--- transforms.py
+++ transforms.py
@@ -6,18 +6,40 @@ import cv2
 import numpy as np
 
 from albumentations import (
-    Normalize,
+    HorizontalFlip,
     PadIfNeeded,
+    RandomCrop,
+    Normalize,
+    Resize,
+    Rotate,
+    ToGray,
+    RandomBrightness,
+    RandomContrast,
+    RandomGamma,
+    CLAHE,
+    Blur,
+    OneOf,
     Compose,
 )
 
+
 def train_transformations(prob=1.0):
-    return Compose([PadIfNeeded(min_height=384, min_width=1248, always_apply=True), 
+    return Compose([
+        RandomCrop(height=128, width=128, always_apply=True),
+        OneOf([HorizontalFlip(p=0.5), Rotate(limit=30, p=0.5)], p=0.5),
+        OneOf([ToGray(p=0.5), 
+            RandomBrightness(p=0.5), 
+            RandomContrast(p=0.5), 
+            RandomGamma(p=0.5), 
+            CLAHE(p=0.5)], p=0.5),
         Normalize(always_apply=True)], p=prob)
 
+
 def valid_tranformations(prob=1.0):
-    return Compose([PadIfNeeded(min_height=384, min_width=1248, value=(0,0,0), always_apply=True),
+    return Compose([
+        PadIfNeeded(min_height=384, min_width=1248, border_mode=cv2.BORDER_CONSTANT, value=(0, 0, 0), always_apply=True),
         Normalize(always_apply=True)], p=prob)
  
+
 def test_trasformations(prob=1.0):
     pass
