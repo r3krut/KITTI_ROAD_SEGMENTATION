5cccd5b0601fc579b95d1017fe8e9786157a4527
r3krut <tananakin.mikhail@gmail.com>
2019-05-08 18:41:39 +0300

    Method for preparation of a hold-out dataset was added.

--- data_processing.py
+++ data_processing.py
@@ -6,6 +6,7 @@ import cv2
 import argparse
 import numpy as np
 from pathlib import Path
+from sklearn.model_selection import train_test_split
 
 from img_utils import (
         getGroundTruth,
@@ -17,6 +18,7 @@ valid_masks_dir = "valid_masks"
 gt_image_2_dir = "gt_image_2"                           #   Ground truth train and validation masks 
 image_2_dir = "image_2"                                 #   RGB images
 droped_valid_image_2_dir = "droped_valid_image_2"       #   source train images without validation areas
+holdout_dir = "hold_out"                                #   this dir contains a hold-out dataset which will be used in evaluation mode.
 
 def prepare_train_valid(path2train):
     """
@@ -92,12 +94,91 @@ def drop_valid(path2train):
         print("Image {0} porcessed.".format(str(src_images_paths[i])))
 
 
+def prepare_holdout_dataset(path2data, test_fraction=0.2):
+    """
+        This method splits a train dataset on train and hold-out data and save this data in a separately directory.
+        Params:
+            path2data       : dir which contains 'training' and 'testing' subdirs.
+            test_fraction   : part of a test data in percent
+    """
+
+    assert test_fraction <= 0.9 and test_fraction > 0, "Error. Invalid test_fraction param: {}".format(test_fraction)
+
+    path2data = Path(path2data)
+    holdout_path = path2data / holdout_dir
+
+    #Creating of hold-out dir and subdirs
+    holdout_path.mkdir(exist_ok=True, parents=True)
+    (holdout_path / 'imgs').mkdir(exist_ok=True, parents=True)
+    (holdout_path / 'masks').mkdir(exist_ok=True, parents=True)
+
+    train_images_paths = path2data / 'training' / droped_valid_image_2_dir
+    train_masks_paths = path2data / 'training' / train_masks_dir
+
+    images_paths = sorted(list(map(str, train_images_paths.glob("*"))))
+    masks_paths = sorted(list(map(str, train_masks_paths.glob("*"))))
+
+    #Urban unmarked
+    uu_imgs_paths = list(filter(lambda p: p.split("/")[-1].startswith("uu_"), images_paths))
+    uu_masks_paths = list(filter(lambda p: p.split("/")[-1].startswith("uu_"), masks_paths))
+
+    #Urban marked
+    um_imgs_paths = list(filter(lambda p: p.split("/")[-1].startswith("um_"), images_paths))
+    um_masks_paths = list(filter(lambda p: p.split("/")[-1].startswith("um_"), masks_paths))
+
+    #Urban multiple mark
+    umm_imgs_paths = list(filter(lambda p: p.split("/")[-1].startswith("umm_"), images_paths))
+    umm_masks_paths = list(filter(lambda p: p.split("/")[-1].startswith("umm_"), masks_paths))
+
+    assert len(uu_imgs_paths) == len(uu_masks_paths), "Error. uu_imgs_paths and uu_masks_paths has differnet length."
+    assert len(um_imgs_paths) == len(um_masks_paths), "Error. um_imgs_paths and um_masks_paths has differnet length."
+    assert len(umm_imgs_paths) == len(umm_masks_paths), "Error. umm_imgs_paths and umm_masks_paths has differnet length."
+
+    #train\holdout uu
+    train_uu_imgs, test_uu_imgs, train_uu_masks, test_uu_masks = train_test_split(uu_imgs_paths, uu_masks_paths, test_size=test_fraction, random_state=111)
+
+    #train\holdout um
+    train_um_imgs, test_um_imgs, train_um_masks, test_um_masks = train_test_split(um_imgs_paths, um_masks_paths, test_size=test_fraction, random_state=111)
+
+    #train\holdout umm
+    train_umm_imgs, test_umm_imgs, train_umm_masks, test_umm_masks = train_test_split(umm_imgs_paths, umm_masks_paths, test_size=test_fraction, random_state=111)
+
+    #Total train
+    total_train_imgs = train_uu_imgs + train_um_imgs + train_umm_imgs
+    total_train_masks = train_uu_masks + train_um_masks + train_umm_masks
+
+    #Total test(hold-out)
+    total_test_imgs = test_uu_imgs + test_um_imgs + test_umm_imgs
+    total_test_masks = test_uu_masks + test_um_masks + test_umm_masks
+
+    if len(list( (holdout_path / 'imgs').glob("*") )) > 0:
+        raise RuntimeError("Dir {} already exists. Possible it contains already prepared hold-out test images.".format(str(holdout_path / 'imgs')))
+
+    if len(list( (holdout_path / 'masks').glob("*") )) > 0:
+        raise RuntimeError("Dir {} already exists. Possible it contains already prepared hold-out test masks.".format(str(holdout_path / 'masks')))
+
+    print("------------INFO-------------")
+    print("Total train images: {}".format(len(total_train_imgs)))
+    print("Moving {} images and {} masks to the hold-out dirs {}, {} respectively".format(len(total_test_imgs), len(total_test_masks), str(holdout_path / 'imgs'), str(holdout_path / 'masks')))
+    print("------------INFO-------------")
+
+    #Moving test images and masks to a hold-out dir
+    for idx in range(0, len(total_test_imgs)):
+        img_path = Path(total_test_imgs[idx])
+        mask_path = Path(total_test_masks[idx])
+
+        #move
+        img_path.rename(holdout_path / 'imgs' / img_path.name)
+        mask_path.rename(holdout_path / 'masks' / mask_path.name)
+
+
 if __name__ == "__main__":
     ap = argparse.ArgumentParser(description="Argument parser for data_processing module")
     ap.add_argument("--train-dir", type=str, required=True, help="Path to a train dir(should contain 'gt_image_2' and 'image_2' subdirs).")
     args = ap.parse_args()
 
-    #prepare_train_valid(args.train_dir)
-    drop_valid(args.train_dir)
+    # prepare_train_valid(args.train_dir)
+    # drop_valid(args.train_dir)
+    prepare_holdout_dataset(path2data=args.train_dir)
 
     print("Done!")
