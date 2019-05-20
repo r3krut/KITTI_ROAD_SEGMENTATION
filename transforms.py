ee46824cc370aa00548716821b218896ea392f9a
r3krut <tananakin.mikhail@gmail.com>
2019-05-20 14:30:12 +0300

    Mods in train transformations.

--- transforms.py
+++ transforms.py
@@ -13,11 +13,21 @@ from albumentations import (
     Resize,
     Rotate,
     ToGray,
+    RandomBrightnessContrast,
     RandomBrightness,
     RandomContrast,
     RandomGamma,
     CLAHE,
     Blur,
+    RandomShadow,
+    IAAAdditiveGaussianNoise,
+    GaussNoise,
+    MotionBlur,
+    MedianBlur,
+    Blur,
+    HueSaturationValue,
+    IAASharpen,
+    IAAEmboss,
     OneOf,
     Compose,
 )
@@ -26,20 +36,25 @@ from albumentations import (
 def train_transformations(prob=1.0):
     return Compose([
         PadIfNeeded(min_height=384, min_width=1280, border_mode=cv2.BORDER_CONSTANT, value=(0, 0, 0), always_apply=True),
-        # RandomCrop(height=256, width=256, always_apply=True),
-        OneOf([HorizontalFlip(p=0.5), Rotate(limit=30, p=0.5)], p=0.5),
-        OneOf([ToGray(p=0.5), 
+        OneOf([HorizontalFlip(p=0.5), Rotate(limit=20, p=0.3)], p=0.5),
+        OneOf([
+            IAAAdditiveGaussianNoise(),
+            GaussNoise(),
+        ], p=0.2),
+        OneOf([ToGray(p=0.3), 
             RandomBrightness(p=0.5), 
             RandomContrast(p=0.5), 
             RandomGamma(p=0.5), 
-            CLAHE(p=0.5)], p=0.5),
+            CLAHE(p=0.5),
+            IAASharpen(p=0.45)], p=0.5),
+        RandomShadow(p=0.4),
+        HueSaturationValue(p=0.3),
         Normalize(always_apply=True)], p=prob)
 
 
 def valid_tranformations(prob=1.0):
     return Compose([
         PadIfNeeded(min_height=384, min_width=1280, border_mode=cv2.BORDER_CONSTANT, value=(0, 0, 0), always_apply=True),
-        # RandomCrop(height=256, width=256, always_apply=True),
         Normalize(always_apply=True)], p=prob)
  
 
