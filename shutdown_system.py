97993fd50f4b11fcbfb8d7fe7cc535fa95394169
r3krut <tananakin.mikhail@gmail.com>
2019-06-04 18:18:09 +0300

    Fixes

--- shutdown_system.py
+++
@@ -1,8 +0,0 @@
-"""
-    This module contains only one function that allows disable the current system.
-"""
-
-from utils import shutdown_system
-
-if __name__ == "__main__":
-    shutdown_system()
